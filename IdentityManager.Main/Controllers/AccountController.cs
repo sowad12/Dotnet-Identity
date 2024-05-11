0acebe494e4c07252888e26d0982716e0e38c915
sowad <ashfaqulislam4566@gmail.com>
2024-05-11 17:24:08 +0600

    register&loginViewPageAdd

---
+++ IdentityManager.Main/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using IdentityManager.Library.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityManager.Main.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            var registerViewModel = new RegisterViewModel();
+            return View(registerViewModel);
+
+        }
+        //[HttpPost]
+        //public async Task<IActionResult> Register()
+        //{
+        //    //var registerViewModel = new RegisterViewModel();
+        //    //return View(registerViewModel);
+        //    return Ok();
+        //}
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            var loginViewModel = new LoginViewModel();
+            return View(loginViewModel);
+
+        }
+    }
+}
